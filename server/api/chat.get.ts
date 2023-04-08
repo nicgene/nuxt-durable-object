07b9d6865dcc13f2f160ab28319e17a887839824
nicgene <dupkey@protonmail.com>
2023-04-08 15:23:07 -0500

    fix: durable object request/response

--- server/api/chat.get.ts
+++ server/api/chat.get.ts
@@ -4,4 +4,9 @@ export default defineEventHandler(async (event) => {
   const { env } = event.context as { env: { CHAT: DurableObjectNamespace } };
   const id = env.CHAT.idFromName(new URL(getRequestURL(event)).pathname);
   const chat = env.CHAT.get(id);
+  const newRequest = new Request(getRequestURL(event));
+  // @ts-ignore https://github.com/cloudflare/workers-sdk/issues/2787
+  const response = await chat.fetch(newRequest);
+
+  return await response.text()
 });
