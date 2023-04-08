87895539fc520b96337e3a4de7a095f950082059
nicgene <dupkey@protonmail.com>
2023-04-08 15:22:45 -0500

    chore: syntax

--- server/api/chat.get.ts
+++ server/api/chat.get.ts
@@ -2,7 +2,6 @@ import { DurableObjectNamespace } from "@cloudflare/workers-types";
 
 export default defineEventHandler(async (event) => {
   const { env } = event.context as { env: { CHAT: DurableObjectNamespace } };
-  let id = env.CHAT.idFromName(new URL(getRequestURL(event)).pathname);
-  let chat = env.CHAT.get(id);
-  return await chat.fetch("/chat");
+  const id = env.CHAT.idFromName(new URL(getRequestURL(event)).pathname);
+  const chat = env.CHAT.get(id);
 });
